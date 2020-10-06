410898611cf324465b420e1fc3626b7bd5094912
GuillaumeChampain <guillaume.champain@epfl.ch>
2020-10-06 18:57:28 +0200

    no more error

--- three_link_biped_start/dynamics/impact.m
+++ three_link_biped_start/dynamics/impact.m
@@ -4,14 +4,14 @@
 % 
 function [q_p, dq_p] = impact(q_m, dq_m)
 
-q_p(1, 1) = pi/6;% angular positions before/after impact
-q_p(2, 1) = -pi/6; 
-q_p(3, 1) = pi/8;
+q_p(1, 1) = q_m(2);% angular positions before/after impact
+q_p(2, 1) = q_m(1); 
+q_p(3, 1) = q_m(3);
 
 A_m=eval_A_m(q_m);
 A_p=eval_A_p(q_p);
 
-dq_p=A_p\(A_m*dq_m);
+dq_p=A_p\A_m*dq_m;
 % A_p dq_p = A_m dq_m
 % Note: To solve the equation Ax = b you could use x = A \ b to avoid taking
 % inverse of A. 
